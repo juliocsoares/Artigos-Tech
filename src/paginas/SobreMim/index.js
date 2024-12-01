00027e22b3c5c83aa8675f5ed6815f03767f5c85
Julio César <julio06.sp@gmail.com>
2024-11-30 23:32:13 -0300

    atualização aba sobre

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -17,25 +17,22 @@ export default function SobreMim() {
                 alt="Minha foto"
                 className={styles.fotoSobreMim} />
 
-            <p className={styles.paragrafo}>
-                Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
-            </p>
-            <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
-            </p>
-            <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
-            </p>
-            <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
-            </p>
-            <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
+<p className={styles.paragrafo}>
+    Oi, tudo bem? Eu sou estagiário de service desk, e quem sabe no futuro, estagiário de desenvolvimento rsrs. Estou feliz de te ver por aqui.
+</p>
+<p className={styles.paragrafo}>
+    Minha jornada em tech começou quando, decidi cursar Sistemas de Informação, onde tive a oportunidade de aprender mais sobre desenvolvimento de software e design de interfaces. Durante a universidade, participei de grupos acadêmicos e estudei muito sobre o mundo de tecnologia, o que consolidou minha paixão pela área.
+</p>
+<p className={styles.paragrafo}>
+    Foi nesse período que comecei a aplicar meus conhecimentos em projetos reais, como a criação de websites e sistemas para otimizar processos. Isso me deu uma experiência mais próxima da realidade profissional.
+</p>
+<p className={styles.paragrafo}>
+    Em 2024 entrei em uma empresa maravilhosa, onde trabalho com service desk em geral, e também algumas automações de processos, utilizando ferramentas como Power Apps, Power Automate e Microsoft Lists. Além disso, também desenvolvi um app no Power Apps para gestão de chamados, o que tem sido uma experiência rica em aprendizado prático.
+</p>
+<p className={styles.paragrafo}>
+    Hoje, também continuo minha jornada de aprendizado constante, aplicando todos esses conhecimentos em meus projetos de UI design e automação. Adoro poder compartilhar essas experiências e conhecimentos com outros profissionais e espero que você aproveite o conteúdo que estou trazendo para você.
+</p>
 
-            </p>
-            <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
-            </p>
         </PostModelo>
     )
 }
